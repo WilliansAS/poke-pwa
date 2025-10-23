c0841276e06d67450b7ae77eeccee5bc0e556095
WilliansAS <anthowill1608@gmail.com>
2025-10-23 13:37:43 -0500

    Implementar Workbox

---
+++ config-overrides.js
@@ -0,0 +1,49 @@
+const { override, addWebpackPlugin } = require("customize-cra");
+const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
+
+module.exports = override(
+  addWebpackPlugin(
+    new WorkboxWebpackPlugin.GenerateSW({
+      clientsClaim: true,
+      skipWaiting: true,
+      // Excluye archivos que no queremos cachear
+      exclude: [
+        /\.map$/,
+        /asset-manifest\.json$/,
+        /robots\.txt/,
+        /LICENSE\.txt/,
+      ],
+
+      // Define cómo cachear cosas que no son parte del build (como la API)
+      runtimeCaching: [
+        {
+          // Cachea las peticiones a la API de Pokémon
+          urlPattern: new RegExp("^https://pokeapi.co/api/v2/pokemon"),
+          // (da la caché rápido, pero busca actualizaciones en segundo plano)
+          handler: "StaleWhileRevalidate",
+          options: {
+            cacheName: "pokeapi-cache",
+            expiration: {
+              maxEntries: 150,
+              maxAgeSeconds: 30 * 24 * 60 * 60,
+            },
+          },
+        },
+        {
+          // Cachea las imágenes de Pokémon
+          urlPattern: new RegExp(
+            "^https://raw.githubusercontent.com/PokeAPI/sprites/"
+          ),
+          handler: "CacheFirst", // Si está en caché, no la vuelve a pedir
+          options: {
+            cacheName: "pokemon-images-cache",
+            expiration: {
+              maxEntries: 200,
+              maxAgeSeconds: 30 * 24 * 60 * 60,
+            },
+          },
+        },
+      ],
+    })
+  )
+);
